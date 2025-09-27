37c184bf8ae63ba9b63fdae884afb39e837a9bd3
Matheus Alves Passos <136806259+MatheusPas@users.noreply.github.com>
2025-09-27 20:10:51 -0300

    Criação do projeto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Trabalho_Database.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
